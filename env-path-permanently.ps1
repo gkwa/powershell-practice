5413ec31306079ecc46820acb3f97deec17b5dfe
taylor <tailor@u.washington.edu>
2014-01-12 15:53:50 -0800

    t

--- env-path-permanently.ps1
+++ env-path-permanently.ps1
@@ -33,12 +33,12 @@ if ($ApplyImmediately)
 {
     if (-not ("Win32.NativeMethods" -as [Type]))
     {
-        # import sendmessagetimeout from win32
-        Add-Type -Namespace Win32 -Name NativeMethods -MemberDefinition @"
+	# import sendmessagetimeout from win32
+	Add-Type -Namespace Win32 -Name NativeMethods -MemberDefinition @"
     [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
     public static extern IntPtr SendMessageTimeout(
-        IntPtr hWnd, uint Msg, UIntPtr wParam, string lParam,
-        uint fuFlags, uint uTimeout, out UIntPtr lpdwResult);
+	IntPtr hWnd, uint Msg, UIntPtr wParam, string lParam,
+	uint fuFlags, uint uTimeout, out UIntPtr lpdwResult);
 "@
     }
 
