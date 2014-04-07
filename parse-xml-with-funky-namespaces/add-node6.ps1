3acb1c6a3128cb0f155824f09f878a1e686f3915
taylor <tailor@u.washington.edu>
2014-04-06 17:02:21 -0700

    yada-yada

--- parse-xml-with-funky-namespaces/add-node6.ps1
+++ parse-xml-with-funky-namespaces/add-node6.ps1
@@ -95,7 +95,7 @@ if(-not $xml.SelectSingleNode("//DiskConfiguration"))
     $NewDiskConfig = $xml.CreateElement("DiskConfiguration", $ns.LookupNamespace("urn"))
     [void]$NewDiskConfig.AppendChild($NewDisk)
 
-    $Component = $xml.unattend.settings.component | where { [string]$_.name -eq "Microsoft-Windows-Setup" }
+    $Component = $xml.unattend.settings.component | where { [string]$_.name -is "Microsoft-Windows-Setup" }
     $Component.AppendChild($NewDiskConfig)
 }
 $xml.Save("${file}.result")
