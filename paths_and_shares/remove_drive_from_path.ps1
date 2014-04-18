00874288a179a9a7fc106bbe8b0d82e7b3e0ea28
taylor <tailor@u.washington.edu>
2014-04-17 17:27:34 -0700

    Add script to test manipulating absolute filesystem paths
    Change-Id: I21d299391aacc8604cf089f1b6cb8461d0198d42

---
+++ paths_and_shares/remove_drive_from_path.ps1
@@ -0,0 +1,7 @@
+split-path -parent $MyInvocation.MyCommand.Definition
+
+split-path $MyInvocation.MyCommand.Definition
+
+split-path $MyInvocation.MyCommand.Definition -qualifier
+
+split-path $MyInvocation.MyCommand.Definition -noqualifier
