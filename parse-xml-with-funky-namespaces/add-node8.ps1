6433d54fb64c303e6226a544f1b2495226812114
taylor <tailor@u.washington.edu>
2014-04-19 21:25:51 -0700

    yada-yada

--- parse-xml-with-funky-namespaces/add-node8.ps1
+++ parse-xml-with-funky-namespaces/add-node8.ps1
@@ -1,3 +1,14 @@
+# Creates this:
+# <component name="Networking-MPSSVC-Svc" processorArchitecture="x86" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+#     <FirewallGroups>
+#         <FirewallGroup wcm:action="add" wcm:keyValue="RemoteDesktop">
+#             <Group>Remote Desktop</Group>
+#             <Profile>all</Profile>
+#             <Active>true</Active>
+#         </FirewallGroup>
+#     </FirewallGroups>
+# </component>
+
 
 
 
@@ -21,20 +32,6 @@ http://social.technet.microsoft.com/Forums/scriptcenter/en-US/405bd5ba-cb35-4ef6
 
 #>
 
-
-
-# Creates this:
-# <component name="Networking-MPSSVC-Svc" processorArchitecture="x86" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-#     <FirewallGroups>
-#         <FirewallGroup wcm:action="add" wcm:keyValue="RemoteDesktop">
-#             <Group>Remote Desktop</Group>
-#             <Profile>all</Profile>
-#             <Active>true</Active>
-#         </FirewallGroup>
-#     </FirewallGroups>
-# </component>
-
-
 $filename = "Unattend.xml"
 [xml]$xml = (gc $filename)
 
@@ -42,27 +39,12 @@ $filename = "Unattend.xml"
 $ns.AddNamespace('urn', "urn:schemas-microsoft-com:unattend")
 $ns.AddNamespace('wcm', "http://schemas.microsoft.com/WMIConfig/2002/State")
 
-# $pass = ($xml.SelectNodes('//urn:settings',$ns) | where { $_.pass -eq 'specialize' })[0]
 $settings = $xml.SelectSingleNode('//urn:settings[@pass="specialize"]', $ns)
 
 if(!($settings.component | where { $_.name -eq 'Networking-MPSSVC-Svc' }))
 {
     $component = $settings.component[0].clonenode($false)
     [void]$component.SetAttribute("name", 'Networking-MPSSVC-Svc')
-
-    $component.outerxml
-
-#    $component.outerxml
-#     $fwgroup=[xml]@"
-# <FirewallGroups>
-#     <FirewallGroup wcm:action="add" wcm:keyValue="RemoteDesktop">
-# 	<Group>Remote Desktop</Group>
-# 	<Profile>all</Profile>
-# 	<Active>true</Active>
-#     </FirewallGroup>
-# </FirewallGroups>
-# "@
-#    $newNode = $xml.ImportNode($fwgroup.DocumentElement,$true)
 
     # <Group>Remote Desktop</Group>
     $group = $xml.CreateElement("Group", $ns.LookupNamespace("urn"))
@@ -93,46 +75,5 @@ if(!($settings.component | where { $_.name -eq 'Networking-MPSSVC-Svc' }))
 
     [void]$settings.appendChild($component)
 }
-
-# $clone_cli = $step_cli.cloneNode($false)
-# [void]$clone_cli.SetAttribute("name", "", "Shutdown computer")
-# [void]$clone_cli.SetAttribute("disable", "", "false")
-# $action = $xml.CreateElement("action")
-# $action.InnerText = "shutdown -t 0 -s"
-# $clone_cli.appendChild($action)
-
-# # Add the shutdown step to the last step of the Capture Image group
-# $group = $xml.SelectNodes('//group') | where { $_.name -eq 'Capture Image' }
-# [void]$group.InsertAfter($clone_cli, $group.LastChild)
-
-# $xml.Save("${file}.result")
-
-
-
-# # <fDenyTSConnections>
-# $NewConn = $xml.CreateElement("fDenyTSConnections", $ns.LookupNamespace("urn"))
-# $NewConn.InnerText = "false"
-
-# # FIXME: why does component[0] have a different namespace than
-# # component[1].  Should we specifically select the component that has
-# # the right namespace according to Windows System Image Manager?
-# $tmp = $xml.unattend.settings[0].component[1]
-# $comp = $tmp.CloneNode($false)
-
-# [void]$comp.AppendChild($NewConn)
-# $comp.name = "Microsoft-Windows-TerminalServices-LocalSessionManager"
-
-# $comp1 = $comp.cloneNode($true)
-# $comp2 = $comp.cloneNode($true)
-# $comp3 = $comp.cloneNode($true)
-
-# # Enable remote desktop during Speclialize phase
-# [void]$xml.SelectSingleNode("//urn:settings[@pass='specialize']", $ns).AppendChild($comp1)
-
-# # Enable remote desktop during Offline Servicing phase
-# [void]$xml.SelectSingleNode("//urn:settings[@pass='offlineServicing']", $ns).AppendChild($comp2)
-
-# # Enable remote desktop during Generlize phase
-# [void]$xml.SelectSingleNode("//urn:settings[@pass='generalize']", $ns).AppendChild($comp3)
 
 $xml.Save("${filename}.result")
