17b23c6f1714f0ea3d3fef08f5377ceb54bb8aa0
taylor <tailor@u.washington.edu>
2014-04-22 00:45:40 -0700

    yada-yada
    Change-Id: I30a248f619b9b5e56e45565fb0e50c848f168a2d

--- ternary/t1.ps1
+++ ternary/t1.ps1
@@ -1,5 +1,14 @@
+<#
+http://blogs.msdn.com/b/powershell/archive/2006/12/29/dyi-ternary-operator.aspx
+#>
+
+#. '.\Invoke-Ternary.ps1'
+
 $quantity=1
 $price=2
 
 $total = ($quantity * $price ) * ({$quantity -le 10} ? {.9} : {.75})
+$total
+
+$total = ($quantity * $price ) * (?:  {$quantity -le 10} {.9} {.75})
 $total
