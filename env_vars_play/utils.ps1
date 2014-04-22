937eb7732fa5bef715d2f5a3e4835bda73d57c63
taylor <tailor@u.washington.edu>
2014-04-21 23:39:21 -0700

    yada-yada

---
+++ env_vars_play/utils.ps1
@@ -0,0 +1,13 @@
+function Update-Environment {
+    $locations = 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment',
+		 'HKCU:\Environment'
+
+    $locations | ForEach-Object {
+	$k = Get-Item $_
+	$k.GetValueNames() | ForEach-Object {
+	    $name  = $_
+	    $value = $k.GetValue($_)
+	    Set-Item -Path Env:\$name -Value $value
+	}
+    }
+}
