6fc460b5e31cf934d5e48e96e9bd4d5b311afc29
Taylor Monacelli <taylormonacelli@gmail.com>
2015-06-10 20:46:24 -0700

    yada-yada

--- window-width-output/t3.ps1
+++ window-width-output/t3.ps1
@@ -3,6 +3,11 @@
 https://goo.gl/f9Y2xx
 http://goo.gl/EwUTBr
 
+Note: The Window Size cannot be larger than the Buffer Size, so the
+Buffer Size’s Width must be increased prior to increasing the Window
+Size’s Width.
+http://goo.gl/6nTAAg
+
 #>
 
 $pshost = get-host
