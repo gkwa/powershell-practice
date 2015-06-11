66ccc395b982136bfe54393167a9b4665da5397d
Taylor Monacelli <taylormonacelli@gmail.com>
2015-06-10 20:46:28 -0700

    yada-yada

---
+++ window-width-output/t4.ps1
@@ -0,0 +1,14 @@
+<#
+
+http://stackoverflow.com/a/1165347/1495086
+
+#>
+
+# Update output buffer size to prevent clipping in Visual Studio output window.
+if( $Host -and $Host.UI -and $Host.UI.RawUI ) {
+  $rawUI = $Host.UI.RawUI
+  $oldSize = $rawUI.BufferSize
+  $typeName = $oldSize.GetType( ).FullName
+  $newSize = New-Object $typeName (500, $oldSize.Height)
+  $rawUI.BufferSize = $newSize
+}
