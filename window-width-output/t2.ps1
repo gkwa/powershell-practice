1f858d2f8e7e2c3e46c112d26b787276321e0adf
Taylor Monacelli <taylormonacelli@gmail.com>
2015-06-10 20:30:26 -0700

    yada-yada

--- window-width-output/t2.ps1
+++ window-width-output/t2.ps1
@@ -1,5 +1,5 @@
+$host.ui.rawui
 $Host.UI.RawUI.BufferSize = New-Object Management.Automation.Host.Size (500, 25)
 
-$host.ui.rawui
 write-verbose "$(get-date)"
 write-verbose $($s="";for($i=1;$i -lt 10;$i+=1) {$s+=([string] $i * 9) + " "};$s*5)
