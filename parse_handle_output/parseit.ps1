1e1d605fa9348b8982ec4a2c2e39d3fa7c734fa3
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-01-29 12:29:27 -0800

    Remove logging module since it uses write-host

--- parse_handle_output/parseit.ps1
+++ parse_handle_output/parseit.ps1
@@ -1,11 +1,4 @@
-. .\logger.ps1
-
 $regex='^(.*)\s+pid: (\d+)\s+type: ([^ ]+)\s+([A-Fa-f0-9]+): (.*)'
-
-$ErrorActionPreference = "SilentlyContinue"
-Stop-Transcript | out-null
-$ErrorActionPreference = "Continue"
-Start-Transcript -path output.txt -append
 
 gc handle-output.txt |
   Select-String $regex -AllMatches |
@@ -15,9 +8,5 @@ gc handle-output.txt |
 	  $_pid=$_.Groups[2].Value.Trim()
 	  $message = "process {0} has handle open on {1}, will kill process {2}" `
 		-f $_.Groups[1].Value.Trim(), $_.Groups[5].Value, $_pid
-	  
-	  Write-Log $message "INFO" 
 	  "taskkill /f /pid $_pid"
-  } >out.txt
-
-Stop-Transcript
+  }
