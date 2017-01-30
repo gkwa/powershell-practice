a73efd71750c0f5c73c64cd3b31b513c72c1727a
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-01-29 21:22:51 -0800

    yada-yada

--- parse_handle_output/parseit.ps1
+++ parse_handle_output/parseit.ps1
@@ -1,35 +1,42 @@
 $regex='^(.*)\s+pid: (\d+)\s+type: ([^ ]+)\s+([A-Fa-f0-9]+): (.*)'
 
-function hasOpenHandle( $vmname )
+function hasOpenHandle($vmname, $handle_out)
 {
-#	gc handle-output.txt |
-	handle $vmname |
-	  Select-String $regex -AllMatches |
-	  Foreach-Object { $_.Matches } |
-	  Foreach-Object {
-		  $pname=$_.Groups[1].Value.Trim()
-		  $_pid=$_.Groups[2].Value.Trim()
-
-		  # dont report handle open if handle.exe has open handle
-		  if($pname -notmatch 'handle.exe') {
-			  return $true
+	foreach( $line in $handle_out ) {
+		$line | Select-String $regex -AllMatches |
+		  Foreach-Object { $_.Matches } |
+		  Foreach-Object {
+			  $pname=$_.Groups[1].Value.Trim()
+			  $_pid=$_.Groups[2].Value.Trim()
 		  }
-	  }
+		# dont report handle open if handle.exe has open handle
+		if($pname -notmatch 'handle(64)?.exe') {
+			return $true
+		}
+	}
 	return $false
 }
 
-gc handle-output.txt |
-  Select-String $regex -AllMatches |
-  Foreach-Object { $_.Matches } |
-  Foreach-Object {
-	  $pname=$_.Groups[1].Value.Trim()
-	  $_pid=$_.Groups[2].Value.Trim()
-	  $message = "process {0} has handle open on {1}, will kill process {2}" `
-		-f $_.Groups[1].Value.Trim(), $_.Groups[5].Value, $_pid
-	  "taskkill /f /pid $_pid # $message"
-  }
+function listHandles($vmname, $handle_out)
+{
+	foreach( $line in $handle_out ) {
+		$line | Select-String $regex -AllMatches |
+		  Foreach-Object { $_.Matches } |
+		  Foreach-Object {
+			  $pname=$_.Groups[1].Value.Trim()
+			  $_pid=$_.Groups[2].Value.Trim()
+			  $fpath=$_.Groups[5].Value.Trim()
+			  "{0} {1} {2} {3}" -f $_pid, [System.IO.Path]::GetExtension($fpath), $pname, $fpath
+			  "taskkill /F /pid $_pid"
+		  }
+	}
+}
 
 $vmname='eval-win8x64-enterprise'
-if(hasOpenHandle $vmname) {
-	"handle to $vmname is open"
-}
+
+# $handle_out = handle $vmname
+# $handle_out = gc handle-output.txt
+$handle_out = handle $vmname
+
+hasOpenHandle $vmname $handle_out
+listHandles $vmname $handle_out
